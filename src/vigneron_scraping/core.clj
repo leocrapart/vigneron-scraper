a6355203737eba5c57f51cba656eec7ee78f8099
Leo Crapart <leocrapart@yahoo.fr>
2022-12-05 22:43:36 +0100

    scraped all posts urls

--- src/vigneron_scraping/core.clj
+++ src/vigneron_scraping/core.clj
@@ -8,17 +8,184 @@
   (println "Hello, World!"))
 
 
-(slurp "https://www.vigneron-independant.com/chateau-des-correges")
 
-; using enlive
+;;;;;;;;;;; SCRAPE URLS
 
-(def url "https://www.vigneron-independant.com/chateau-des-correges")
+; suite : 
+; scrape urls of pages => url list
+; count 
+; scrape (1 sec per page)
 
-(def content
-  (html/html-resource (java.net.URL. url)))
+; fix page scraping for page-data with blanks : see page and upgrade alg
+
+(def url-search "https://www.vigneron-independant.com/search-vigneron")
+
+(def content-search
+  (html/html-resource (java.net.URL. url-search)))
+
+; #search-right > div:nth-child(2) > div.result-name > a
+;  [:div (html/nth-child 2)] :div.result-name :a
+(defn url [content]
+  (first (:content (first 
+      (html/select content [:body])))))
+
+(url content-search)
+
+content-search
+
+; n : page-number (page-2, page-3, ...)
+; x : post-number
+
+(defn search-page [n]
+  (html/html-resource (str "page-" n ".html")))
+
+; #search-right > div:nth-child(2) > div.result-name > a
+
+; boosted search-page fn
+(defn x-page [n]
+  (slurp (str "resources/page-" n ".html")))
+
+
+; (defn url-x [n x]
+;   (:href
+;   (:attrs
+;   (first
+;   (html/select (search-page n) [:#search-right [:div (html/nth-child (+ 1 x))] :div.result-name :a])))))
+
+; boosted
+
+(defn url-x [n x]
+  (let [string-x ((clojure.string/split (x-page n) #"\"result-name\"><a href=\"") x)]
+    (first
+      (clojure.string/split string-x #"\">"))))
+
+; (url-x 58 1)
+
+(def first-60-naturals (map inc (range 60)))
+
+(defn url-function-for-page-2 [x]
+  ((partial url-x 2) x))
+
+(defn url-function-for-page-n [n x]
+  ((partial url-x n) x))
+
+(defn urls [n]
+  (map (partial url-function-for-page-n n) first-60-naturals))
+
+
+(defn inject-new-line [a b]
+  (str a "\n" b))
+
+(defn inject-new-lines [seq]
+  (reduce inject-new-line seq))
+
+(defn save-seq-into-txt-file [file-name seq]
+  (spit file-name (inject-new-lines seq)))
+
+
+
+(defn scrape-urls [n]
+  (save-seq-into-txt-file (str "urls-" n ".txt") (urls n)))
+
+; (scrape-urls 9)
+
+; last one (98)
+; modify first-60-naturals to the number of posts on last page
+; and run this
+; then put number back to 60 for first-60-naturals
+; (save-seq-into-txt-file (str "urls-" 98 ".txt") (urls 98))
+; (save-seq-into-txt-file (str "urls-" 0 ".txt") (urls 0))
+
+
+; (drop 10 (map inc (range 100)))
+
+(defn scrape-all-urls []
+  (let [first-98 (map inc (range 98))
+        from (drop 10 first-98)]
+    (map scrape-urls from)))
+
+(scrape-all-urls)
+
+
+
+
+;;;;;;;; CONCATENATE
+
+(def first-98-naturals
+  (range 99))
+
+(defn add-new-line [urls]
+  (str urls "\n"))
+
+(defn retrieve-urls [n]
+  (add-new-line
+    (slurp (str "urls-" n ".txt"))))
+
+(defn all-urls-seq []
+  (map retrieve-urls first-98-naturals))
+
+(count (all-urls-seq))
+
+
+(defn concatenate-txt []
+  (save-seq-into-txt-file "urls.txt" (all-urls-seq)))
+
+(concatenate-txt)
+
+
+
+
+
+
+;;;;;;; READ
+
+;;; get urls into a vector
+; filter empty due to the new line in the text file for clarity
+; at each 60 urls
+(defn not-empty? [thing]
+  (not (empty? thing)))
+(not-empty? "")
+
+(defn remove-empty-strings [vec]
+  (filter not-empty? vec))
+
+(def url-list 
+  (remove-empty-strings
+    (clojure.string/split (slurp "urls.txt") #"\n")))
+url-list
+
+(count url-list)
+
+
+
+
+
+;;;;;;;;;; SCRAPE PAGE WITH URL
 
 ; stickit-Contact = :body :section :div [:div (html/nth-child 3)] :div :div :div :div [:div (html/nth-child 7)]
 
+(def sticky-contact
+  [:body :section :div [:div (html/nth-child 3)] :div :div :div :div [:div (html/nth-child 7)]])
+
+#stickit-Contact
+/html/body/section/div/div[3]/div/div/div/div/div[7]
+/html/body/section/div/div[3]/div/div/div/div/div[8]
+
+(def page-pic 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/domaine-du-pic")))
+
+(def jeune 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/champagne-lejeune-pere-et-fils")))
+
+(def delaunay 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/domaine-delaunay-1")))
+
+(def fontan 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/vignobles-fontan")))
 
 (defn domain-name [content]
   (first (:content
@@ -56,12 +223,110 @@
       (first 
         (html/select content [:body :section :div [:div (html/nth-child 3)] :div :div :div :div [:div (html/nth-child 7)] :div.establishment-contact :div :div.tel :span])))))
 
+
+; version 2 for https://www.vigneron-independant.com/domaine-du-pic
+
+(defn street-address-2 [content]
+  (second
+    (:content
+      (first
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div :div.adr :div))))))
+
+(defn postal-code-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div :div.adr :span.postal-code))))))
+
+(defn locality-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div :div.adr :span.locality))))))
+
+(defn name-2-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div :div.fn.org :div :div :div))))))
+
+
+(defn mobile-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div [:div (html/nth-child 4)] :span))))))
+
+(mobile-2 fontan)
+
+(html/select fontan 
+    (conj sticky-contact :div.establishment-contact :div [:div (html/nth-child 4)] :span))
+
+(defn tel-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div [:div (html/nth-child 3)] :span))))))
+
+(defn fax-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div [:div (html/nth-child 5)] :span))))))
+
+(defn website-2 [content]
+  (first (:content
+      (first 
+        (html/select content 
+          (conj sticky-contact :div.establishment-contact :div :div.url :a))))))
+
+
 (defn website [content]
   (first (:content
       (first (html/select content [:body :section :div [:div (html/nth-child 3)] :div :div :div :div [:div (html/nth-child 7)] :div.establishment-contact :div :div.url :a])))))
 
+(def page-pic 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/domaine-du-pic")))
+
+(def jeune 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/champagne-lejeune-pere-et-fils")))
+
+(def delaunay 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/domaine-delaunay-1")))
+
+(def fontan 
+  (html/html-resource 
+    (java.net.URL. "https://www.vigneron-independant.com/vignobles-fontan")))
 
 
+(name-2-2 page-pic)
+(street-address-2 page-pic)
+(postal-code-2 page-pic)
+(locality-2 page-pic)
+(mobile-2 page-pic)
+(tel-2 page-pic)
+(fax-2 page-pic)
+(website-2 page-pic)
+
+(def page delaunay)
+
+(name-2-2 delaunay)
+(street-address-2 page)
+(postal-code-2 page)
+(locality-2 page)
+(mobile-2 page) 
+(tel-2 page)
+(fax-2 page)
+(website-2 page)
+
+(mobile delaunay)
+(mobile-2 delaunay)
+(mobile fontan)
+(mobile-2 fontan)
 
 (defn wine-name [content] 
   (first (:content (first 
@@ -143,7 +408,10 @@
 
 (excel-row (page-data url))
 
-(def url-list
+
+;;;;;;;;;;;;;; SCRAPE PAGES WITH URLS
+
+(def url-list-manual
   [ ; page 1
    "https://www.vigneron-independant.com/champagne-lejeune-pere-et-fils"
    "https://www.vigneron-independant.com/domaine-du-val-de-gilly"
@@ -170,34 +438,48 @@
 
 (count url-list)
 
-; scrapes the pages
-(def data
-  (vec
-    (map page-data url-list)))
 
+; scrapes the pages
+(defn data [n]
+  (vec
+    (map page-data (take n url-list))))
+
+(data 10)
+(data 20)
+(data 30)
+(data 40)
+(data 50)
+(count (data 20))
 
 (def title-list 
   ["domain-name" "name-1" "name-2" "street-address" "postal-code" "locality" "mobile" "website" "wine-name" "wine-place" "wine-designation" "wine-domain" "wine-type" "wine-color"])
 
-(defn for-excel []
+(defn for-excel [data]
   `[~title-list ~@(vec (map excel-row data))])
 
-(count for-excel)
+; (count (for-excel data))
 
 ; to excel
 (use 'dk.ative.docjure.spreadsheet)
+(defn save-excel [data]
+  ;; Create a spreadsheet and save it
+  (let [wb (create-workbook "vigneron data"
+                            ; [["domain-name" "name-1" "name-2" "street-address" "postal-code" "locality" "mobile" "website" "wine-name" "wine-place" "wine-designation" "wine-domain" "wine-type" "wine-color"]
+                            ;  (excel-row (data 0))
+                            ;  (excel-row (data 1))
+                            ;  ]
+                            (for-excel data)
+                            )
+        sheet (select-sheet "vigneron data" wb)
+        header-row (first (row-seq sheet))]
+    (set-row-style! header-row (create-cell-style! wb {:background :yellow,
+                                                       :font {:bold true}}))
+    (save-workbook! "scraped-data.xlsx" wb)))
 
-;; Create a spreadsheet and save it
-(let [wb (create-workbook "vigneron data"
-                          ; [["domain-name" "name-1" "name-2" "street-address" "postal-code" "locality" "mobile" "website" "wine-name" "wine-place" "wine-designation" "wine-domain" "wine-type" "wine-color"]
-                          ;  (excel-row (data 0))
-                          ;  (excel-row (data 1))
-                          ;  ]
-                          (for-excel)
-                          )
-      sheet (select-sheet "vigneron data" wb)
-      header-row (first (row-seq sheet))]
-  (set-row-style! header-row (create-cell-style! wb {:background :yellow,
-                                                     :font {:bold true}}))
-  (save-workbook! "scraped-data.xlsx" wb))
+(defn scrape-and-save-excel []
+  (let [data (data 500)]
+    (save-excel data)))
 
+(scrape-and-save-excel)
+
+;;;; test zone
